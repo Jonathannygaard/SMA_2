0d07fa8f865e225a1539c77ab3da5991ea5c3a34
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2024-09-17 20:40:49 +0200

    Comments on controls

--- SMA_2/SMA_2.cpp
+++ SMA_2/SMA_2.cpp
@@ -1,6 +1,8 @@
 
 #include "Core/Application.h"
 
+//Controls: W: Forward, S: Backward, A: Left, D: Right, E: Up, Q: Down, Mouse: Look Around Left Click: Push ball (if in range)
+
 int main(int argc, char* argv[])
 {
     Application app;
